5fbcb44db0be0d1fba17695dbe51fa787e337289
villivald <maxim.villivald@gmail.com>
2021-12-01 18:45:02 +0200

    mean tutorial: parts 1-2

---
+++ Coursework/MEAN/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+//Database Connection
+mongoose.connect(config.database);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database " + config.database);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Database error " + err);
+});
+
+const app = express();
+const users = require("./routes/users");
+const port = 3000;
+
+//Middlewares
+app.use(cors()); //CORS
+app.use(express.static(path.join(__dirname, "public"))); //Serve static files
+app.use(bodyParser.json()); //Body Parser
+app.use("/users", users);
+
+//Index route
+app.get("/", (req, res) => {
+  res.send("Invalid endpoint");
+});
+
+app.listen(port, () => console.log(`Server started on port: ${port}`));
