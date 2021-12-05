d52f6fbf6f6339200b47f4b2a0813e6d5b7dd48d
villivald <maxim.villivald@gmail.com>
2021-12-05 13:38:37 +0200

    mean tutorial: part 8

--- Coursework/MEAN/angular-src/src/app/components/navbar/navbar.component.ts
+++ Coursework/MEAN/angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'flash-messages-angular';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private flashMessage: FlashMessagesService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.show('You are logged out', {
+      cssClass: 'alert-success',
+      timeout: 3000,
+    });
+    this.router.navigate(['/login']);
+    return false;
   }
-
 }
--- Coursework/MEAN/angular-src/src/app/services/auth.service.ts
+++ Coursework/MEAN/angular-src/src/app/services/auth.service.ts
@@ -18,4 +18,27 @@ export class AuthService {
       .post('http://localhost:3000/users/register', user, { headers: headers })
       .pipe(map((res: Response) => res));
   }
+
+  authenticateUser(user) {
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return this.http
+      .post('http://localhost:3000/users/authenticate', user, {
+        headers: headers,
+      })
+      .pipe(map((res: Response) => res));
+  }
+
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 }
